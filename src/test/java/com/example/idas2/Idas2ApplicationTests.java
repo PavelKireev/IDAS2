489abc374296fe6a08426f9e884261a9926f7555
pavelkireev <pkireev97@gmail.com>
2021-10-18 18:57:43 +0200

    1

---
+++ src/test/java/com/example/idas2/Idas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idas2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
